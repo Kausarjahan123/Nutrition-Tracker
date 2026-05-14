f18a1777b8286cf90f05e860625d06f662c4f3f8
KAUSAR JAHAN <jkausar2024@gmail.com>
2026-05-14 11:28:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ st.title("🥗 AI Nutrition Tracker")
 st.write("Search foods from USDA database and get accurate nutrition facts.")
 
 # ---------------- USDA API ---------------- #
-API_KEY = "YOUR_USDA_API_KEY"
+API_KEY = "P73wIVJPiCNTOFtegbe97NOAyX8cCif4fDzCwk07"
 
 # ---------------- SEARCH FUNCTION ---------------- #
 def search_food(food_name):
